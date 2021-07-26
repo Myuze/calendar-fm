2a30e112b5bac3d650f065ae38f0c70ae39e0655
Florian Meneses <florian.meneses@gmail.com>
2021-07-26 01:55:29 -0700

    Added dynamic event cell coloring

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,12 @@
+// Global Definitions
 var hoursInDay = 24;
-var hourStart = 8;
+var hourStart = 0;
 var containerEl = $('.container');
 
 // Show current date and time
 setInterval( function() {
-  var currentDay = moment().format('MM/DD/YYYY hh:mm:ss a');
-  $('#currentDay').text(currentDay)
+  var time = moment().format('MM/DD/YYYY hh:mm:ss a');
+  $('#currentDay').text(time)
 }, 1000);
 
 // Create table based on hours in the day
@@ -13,10 +14,22 @@ var tableEl = $('<table>').addClass('table table-bordered');
 var tBodyEl = $('<tbody>');
 
 for (hour = 0 + hourStart; hour < hoursInDay + hourStart; hour++) {
+  var cellHour = moment().subtract(moment().format('h') - hour, 'h')
   var tRowEl = $('<tr>');
-  var tHeadEl = $('<th>').attr('scope', 'row').text(moment().subtract(moment().format('h') - hour, 'h').format('hh:00 a'));
+  var tHeadEl = $('<th>').attr('scope', 'row').text(cellHour.format('hh:00 a'));
   var tRowEventEl = $('<td>');
   var tRowLockEl = $('<td>');
+
+  // Color Cells depending on time
+  if (moment(cellHour).isBefore(moment(), 'h')) {
+    tRowEventEl.addClass('past');
+
+  } else if (moment(cellHour).isSame(moment(), 'h')) {
+    tRowEventEl.addClass('present');
+
+  } else if (moment(cellHour).isAfter(moment(), 'h')) {
+    tRowEventEl.addClass('future')
+  }
   
   tRowEl.append(tHeadEl);
   tRowEl.append(tRowEventEl);
@@ -32,12 +45,9 @@ containerEl.append(tableEl);
 
 
 // Color code the hours in the event using bootstrap based off the current time
+$('tbody').children().eq(5).addClass('present')
 $('tr').children('td').addClass('table-secondary')
 
 // Clicking on the table row triggers an bootstrap enter event modal
 
 // The Event is stored in localstorage
-
-
-
-  
