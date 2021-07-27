2977cef657a7c736bd304ece2bf6a0db9e0511b0
Florian Meneses <florian.meneses@gmail.com>
2021-07-27 02:08:26 -0700

    updated font sizes and class naming

--- js/javascript.js
+++ js/javascript.js
@@ -32,15 +32,15 @@ setInterval( function() {
 }, 1000);
 
 // Create table based on hours in the day
-var tableEl = $('<table>').addClass('table table-bordered');
+var tableEl = $('<table>').addClass('table');
 var tBodyEl = $('<tbody>');
 
 for (hour = 0 + hourStart; hour < hoursInDay + hourStart; hour++) {
   var cellHour = moment().subtract(moment().format('HH') - hour, 'H')
-  var tRowEl = $('<tr>');
-  var tHeadEl = $('<th>').attr('scope', 'row').addClass('rounded-start rounded-lg').text(cellHour.format('hh:00 a'));
-  var tRowEventEl = $('<td>').addClass('col-event data-editable').attr('contenteditable', true);
-  var tRowLockEl = $('<td>').addClass('col-lock save-btn');
+  var tRowEl = $('<tr>').addClass('time-block');
+  var tHeadEl = $('<th>').attr('scope', 'row').addClass('hour').text(cellHour.format('hh:00 A'));
+  var tRowEventEl = $('<td>').addClass('col-event text-wrap data-editable').attr('contenteditable', true);
+  var tRowLockEl = $('<td>').addClass('col-lock saveBtn').text('🔒');
 
   // Color Cells depending on time
   if (moment(cellHour).isBefore(moment(), 'h')) {
